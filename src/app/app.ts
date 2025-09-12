789c75ce2d7284db0e05bab08f9f3a726c8d779d
thiercelin-loic <thiercelin.loic1@gmail.com>
2025-09-12 16:50:04 +0200

    Refactor App component to include CommonModule and Splash, and implement a splash screen with a 3-second loading time

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,26 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Splash } from './scripts/splash';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    Splash,
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
   protected readonly title = signal('pwhui');
+  protected showSplash = true;
+
+  constructor() {
+    setTimeout(() => {
+      this.showSplash = false;
+    }, 3000); // Simulate a 3-second loading time
+  }
 }
