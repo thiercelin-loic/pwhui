ac6859a5e68e9ee68fdf6f017c368301044b03cf
Loïc Thiercelin <thiercelin.loic1@gmail.com>
2025-10-15 11:59:36 +0200

    feat: Add API URL constants for authentication and user services

---
+++ src/app/server/index.ts
@@ -0,0 +1,5 @@
+const API_HOST = 'http://localhost';
+
+export const AUTH_API_URL = `${API_HOST}:3001/auth`;
+export const USERS_API_URL = `${API_HOST}:3001/users`;
+export const BOOKING_API_URL = `${API_HOST}:3002`;
