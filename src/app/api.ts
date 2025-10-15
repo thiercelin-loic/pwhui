3ad18a8d6750d94606a6a1b3729216aa256ed077
Loïc Thiercelin <thiercelin.loic1@gmail.com>
2025-10-15 11:57:00 +0200

    fix: Remove deprecated API configuration file

--- src/app/api.ts
+++
@@ -1,5 +0,0 @@
-const API_HOST = 'http://pwh.deversion.eu';
-
-export const AUTH_API_URL = `${API_HOST}:3001/auth`;
-export const USERS_API_URL = `${API_HOST}:3001/users`;
-export const BOOKING_API_URL = `${API_HOST}:3002`;
