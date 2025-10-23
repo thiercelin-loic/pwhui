6d1ffe85321f5a45e39b5667fa0118164d3b45ba
Loïc Thiercelin <thiercelin.loic1@gmail.com>
2025-10-23 14:41:50 +0200

    feat: Revamp settings page layout and enhance placeholder functionality with typing effect

--- src/app/settings/settings.ts
+++ src/app/settings/settings.ts
@@ -8,9 +8,62 @@ import { PolicyService } from '../policy/policy.service';
   styleUrl: './settings.css',
 })
 export class Settings {
-  constructor(private policyService: PolicyService) {}
+  constructor(private policyService: PolicyService) { }
+  private option = { month: 'long' } as const;
+  public date: Date = new Date();
+  public today: number = this.date.getDate();
+  public month: string = this.date.toLocaleString('default', this.option);
+  public year: number = this.date.getFullYear();
 
-  openPolicy() {
-    this.policyService.open();
+  private text: number = 0;
+  private char: number = 0;
+
+  private typing: number = 50;
+  private erasing: number = 50;
+  private delay: number = 2000;
+  private interval: any;
+  public placeholder: string = '';
+  private tips: string[] = [
+    'Privacy policy',
+  ];
+
+  private erase() {
+    this.interval = setInterval(() => {
+      if (this.placeholder.length > 0) {
+        this.placeholder = this.placeholder.slice(0, -1);
+      } else {
+        clearInterval(this.interval);
+        this.text = (this.text + 1) % this.tips.length;
+        this.char = 0;
+        this.write();
+      }
+    }, this.erasing);
   }
+
+  private type() {
+    const current = this.tips[this.text];
+
+    if (this.char < current.length) {
+      this.placeholder += current.charAt(this.char);
+      this.char++;
+    } else {
+      clearInterval(this.interval);
+      setTimeout(() => this.erase(), this.delay);
+    }
+  }
+
+  private write(): void {
+    this.interval = setInterval(() =>
+      this.type(),
+      this.typing
+    );
+  }
+
+  public ngOnInit() {
+    this.write();
+  }
+
+  public ngOnDestroy() { clearInterval(this.interval); }
+
+  public openPolicy() {this.policyService.open();}
 }
