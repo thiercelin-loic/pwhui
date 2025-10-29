3178e283d4247447a246f0c9c20eb66d2483b830
Loïc Thiercelin <thiercelin.loic1@gmail.com>
2025-10-29 17:15:57 +0100

    feat: Add content wrapper class to HTML and update routing for Cart component with AuthGuard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,14 @@
 import { Routes } from '@angular/router';
 import { Contract } from './contract/contract';
 import { Landing } from './landing/landing';
+import { Cart } from './cart/cart';
 import { Settings } from './settings/settings';
+import { AuthGuard } from './auth/auth.guard';
 
 export const routes: Routes = [
   { path: '', component: Landing },
   { path: 'auth', loadChildren: () => import('./auth/auth.routes').then(m => m.routes) },
   { path: 'contract', component: Contract },
-  {path: 'settings', component: Settings}
+  { path: 'cart', component: Cart, canActivate: [AuthGuard] },
+  { path: 'settings', component: Settings }
 ];
