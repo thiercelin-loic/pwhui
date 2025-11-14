bb6e38e3a161d7e9afab4f127d78839413650c3c
Loïc Thiercelin <thiercelin.loic1@gmail.com>
2025-11-14 16:30:54 +0100

    chore: Update server paths for chat and booking services

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,5 +2,6 @@ const root = 'http://localhost';
 export const path = {
     auth: `${root}:3001/auth`,
     users: `${root}:3001/users`,
-    booking: `${root}:3002`
+    booking: `${root}:3002`,
+    chat: `${root}:3003`
 }
