b3c3d6a1ad0e0c82abae032f35e034de01558af8
Loïc Thiercelin <thiercelin.loic1@gmail.com>
2025-11-14 16:29:35 +0100

    refactor: Clean up imports and add BookingService to app component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,16 @@
 import { Routes } from '@angular/router';
 import { Contract } from './contract/contract';
 import { Landing } from './landing/landing';
-import { Cart } from './cart/cart';
-import { Settings } from './settings/settings';
 import { AuthGuard } from './auth/auth.guard';
+import { Cart } from './cart/cart';
+import { Chat } from './chat/chat';
+import { Settings } from './settings/settings';
 
 export const routes: Routes = [
   { path: '', component: Landing },
   { path: 'auth', loadChildren: () => import('./auth/auth.routes').then(m => m.routes) },
   { path: 'contract', component: Contract },
   { path: 'cart', component: Cart, canActivate: [AuthGuard] },
+  {path: 'chat', component: Chat, canActivate: [AuthGuard]},
   { path: 'settings', component: Settings }
 ];
