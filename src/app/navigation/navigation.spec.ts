b52ee66c917810d2deef530189eeaa5d96ce3cac
Loïc Thiercelin <thiercelin.loic1@gmail.com>
2025-12-24 20:45:40 +0100

    test: Add unit tests for Navigation component

--- src/app/navigation/navigation.spec.ts
+++ src/app/navigation/navigation.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { Navigation } from './navigation';
 
 describe('Navigation', () => {
@@ -8,7 +10,11 @@ describe('Navigation', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Navigation, RouterTestingModule]
+      imports: [Navigation, RouterTestingModule],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
     })
     .compileComponents();
 
