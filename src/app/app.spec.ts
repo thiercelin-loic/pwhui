91a9054d613d837ffd72024adbbfc573518cedc5
Loïc Thiercelin <thiercelin.loic1@gmail.com>
2026-01-11 20:50:19 +0100

    feat: Add translation module to app component tests for internationalization support

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -2,11 +2,25 @@ import { TestBed } from '@angular/core/testing';
 import { App } from './app';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpLoaderFactory } from './shared/translate-loader';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App, HttpClientTestingModule, RouterTestingModule],
+      imports: [
+        App, 
+        HttpClientTestingModule, 
+        RouterTestingModule,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [HttpClient]
+          }
+        })
+      ],
     }).compileComponents();
   });
 
