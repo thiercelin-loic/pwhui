7a46f853d6dbf66689c94cb3f69e003a17deb892
Loïc Thiercelin <thiercelin.loic1@gmail.com>
2026-03-20 17:33:17 +0100

    feat: update TranslateHttpLoader to include cache buster in translation requests

--- src/app/shared/translate-loader.ts
+++ src/app/shared/translate-loader.ts
@@ -3,14 +3,15 @@ import { TranslateLoader } from '@ngx-translate/core';
 import { Observable } from 'rxjs';
 
 export class TranslateHttpLoader implements TranslateLoader {
-  constructor(private http: HttpClient, public prefix = 'languages/', public suffix = '.json') {}
+  constructor(private http: HttpClient, public prefix = './languages/', public suffix = '.json') {}
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   public getTranslation(lang: string): Observable<any> {
-    return this.http.get(`${this.prefix}${lang}${this.suffix}`);
+    const cacheBuster = new Date().getTime();
+    return this.http.get(`${this.prefix}${lang}${this.suffix}?v=${cacheBuster}`);
   }
 }
 
 export function HttpLoaderFactory(http: HttpClient): TranslateLoader {
-  return new TranslateHttpLoader(http, 'languages/', '.json');
+  return new TranslateHttpLoader(http, './languages/', '.json');
 }
