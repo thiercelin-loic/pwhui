ef3268c4a6188004125c6fceff78d439b6813fa8
Loïc Thiercelin <thiercelin.loic1@gmail.com>
2026-03-20 17:34:54 +0100

    style: format Listings and Bookings interfaces for consistency

--- src/app/landing/landing.model.ts
+++ src/app/landing/landing.model.ts
@@ -1,18 +1,18 @@
 export interface Listings {
-    id: number;
-    name: string;
-    description: string;
-    location: string;
-    amenities: string;
-    photo: string;
-    pricing: number;
+  id: number;
+  name: string;
+  description: string;
+  location: string;
+  amenities: string;
+  photo: string;
+  pricing: number;
 }
 
 export interface Bookings {
-    id: number;
-    listing: number;
-    user: string;
-    arrival: string;
-    departure: string;
-    confirmation: string;
+  id: number;
+  listing: number;
+  user: string;
+  arrival: string;
+  departure: string;
+  confirmation: string;
 }
