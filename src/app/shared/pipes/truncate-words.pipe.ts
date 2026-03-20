27fd61538f6a87ed522f6e983e4c4c81726a5247
Loïc Thiercelin <thiercelin.loic1@gmail.com>
2026-03-20 17:34:35 +0100

    feat: add TruncateWordsPipe for limiting word count in strings

---
+++ src/app/shared/pipes/truncate-words.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'truncateWords',
+  standalone: true,
+})
+export class TruncateWordsPipe implements PipeTransform {
+  transform(value: string | undefined, limit = 1): string {
+    if (!value) {
+      return '';
+    }
+    const words = value.split(' ');
+    if (words.length > limit) {
+      return words.slice(0, limit).join(' ');
+    }
+    return value;
+  }
+}
